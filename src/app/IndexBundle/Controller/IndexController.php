1b8d0f9c68c7fe1440bbcb7a1c6a7d9cd07cd0cd
MiguelCastilloLloret <miguel.castillolloret@alum.uca.es>
2015-12-11 16:01:18 +0100

    Añadida la funcionalidad de modificar y eliminar personajes

--- src/app/IndexBundle/Controller/IndexController.php
+++ src/app/IndexBundle/Controller/IndexController.php
@@ -93,8 +93,19 @@ class IndexController extends Controller{
         }
     }
 
-    public function DD35Action(Request $request){ 
+    public function DD35Action(Request $request){
+
+        $html = $this->container->get('templating')->render(
+            'index/rolManage.html.twig', array('hola' => '', 'juego' => 'DD35Master')
+        );
+
+        return new Response($html);
+    }
+
+    public function DD35CreateAction(Request $request){ 
         $hola = "";
+        $tipo = "";
+        $link = "crear";
         $em = $this->get('doctrine.orm.default_entity_manager');
 
         $List = $em->createQuery('SELECT p.ID, p.nombre FROM app\IndexBundle\Entity\DD35\Plantilla p ORDER BY p.ID ASC')->getResult();
@@ -175,14 +186,167 @@ class IndexController extends Controller{
         }
 
         $html = $this->container->get('templating')->render(
-            'index/masterDD35.html.twig', array('plantilla' => $plantilla->createView(),'form' => $var->createView(), 'hola' => $hola)
+            'index/masterDD35.html.twig', array('plantilla' => $plantilla->createView(),'form' => $var->createView(), 'hola' => $hola, 'tipo' => $tipo, 'link' => $link)
         );
 
         return new Response($html);
     }
 
-    public function VampiroAction(Request $request){ 
+    public function DD35ChangeAction(Request $request){ 
         $hola = "";
+        $tipo = "oculto";
+        $link = "modificar";
+        $em = $this->get('doctrine.orm.default_entity_manager');
+
+        $List = $em->createQuery('SELECT p.ID, p.nombre FROM app\IndexBundle\Entity\DD35\Plantilla p ORDER BY p.ID ASC')->getResult();
+
+        for($i=0;$i<count($List);$i++){
+            $aux = $List[$i]['nombre'];
+            $plList[$List[$i]['ID']] = $aux;
+        }
+
+        $id = new idPlantilla();
+        $plantilla = $this->createFormBuilder($id)
+            ->add('id', 'choice', array('choices' => $plList, 'required' => true))
+            ->getForm();
+
+        $pj = new Personaje();
+        $var = $this->createFormBuilder($pj)
+            ->add('nombre')
+            ->add('clase')
+            ->add('nivel')
+            ->add('puntosVida')
+            ->add('claseArmadura')
+            ->add('bonusFuerza')
+            ->add('bonusDestreza')
+            ->add('bonusConstitucion')
+            ->add('bonusInteligencia')
+            ->add('bonusSabiduria')
+            ->add('bonusCarisma')
+            ->add('salvacionFortaleza')
+            ->add('salvacionVoluntad')
+            ->add('salvacionReflejos')
+            ->add('estado')
+            ->add('resistenciaMagica')
+            ->add('reduccionDano')
+            ->add('arma')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            if(isset($request->request->all()['form']['id'])){
+                $plantilla->bind($request);
+                if($plantilla->isValid()){
+                    $personajePlantilla = $em->getRepository('app\IndexBundle\Entity\DD35\Plantilla')->find($id->id);
+                    $pj = $personajePlantilla;
+                    $tipo = "";
+                    $var = $this->createFormBuilder($pj)
+                        ->add('nombre')
+                        ->add('clase')
+                        ->add('nivel')
+                        ->add('puntosVida')
+                        ->add('claseArmadura')
+                        ->add('bonusFuerza')
+                        ->add('bonusDestreza')
+                        ->add('bonusConstitucion')
+                        ->add('bonusInteligencia')
+                        ->add('bonusSabiduria')
+                        ->add('bonusCarisma')
+                        ->add('salvacionFortaleza')
+                        ->add('salvacionVoluntad')
+                        ->add('salvacionReflejos')
+                        ->add('estado')
+                        ->add('resistenciaMagica')
+                        ->add('reduccionDano')
+                        ->add('arma')
+                        ->getForm();
+                    }
+            }
+            else{
+                $var->bind($request);
+                if($var->isValid()){
+                    $em->flush();
+                    $hola = "Se modificó correctamente el personaje en la BD";
+                }
+            }
+        }
+
+        $html = $this->container->get('templating')->render(
+            'index/masterDD35.html.twig', array('plantilla' => $plantilla->createView(),'form' => $var->createView(), 'hola' => $hola, 'tipo' => $tipo, 'link' => $link)
+        );
+
+        return new Response($html);
+    }
+    
+    public function DD35DeleteAction(Request $request){ 
+        $hola = "";
+        $tipo = "oculto";
+        $link = "eliminar";
+        $em = $this->get('doctrine.orm.default_entity_manager');
+
+        $List = $em->createQuery('SELECT p.ID, p.nombre FROM app\IndexBundle\Entity\DD35\Personaje p ORDER BY p.ID ASC')->getResult();
+
+        for($i=0;$i<count($List);$i++){
+            $aux = $List[$i]['nombre'];
+            $plList[$List[$i]['ID']] = $aux;
+        }
+
+        $id = new idPlantilla();
+        $plantilla = $this->createFormBuilder($id)
+            ->add('id', 'choice', array('choices' => $plList, 'required' => true))
+            ->getForm();
+
+        $pj = new Personaje();
+        $var = $this->createFormBuilder($pj)
+            ->add('nombre')
+            ->add('clase')
+            ->add('nivel')
+            ->add('puntosVida')
+            ->add('claseArmadura')
+            ->add('bonusFuerza')
+            ->add('bonusDestreza')
+            ->add('bonusConstitucion')
+            ->add('bonusInteligencia')
+            ->add('bonusSabiduria')
+            ->add('bonusCarisma')
+            ->add('salvacionFortaleza')
+            ->add('salvacionVoluntad')
+            ->add('salvacionReflejos')
+            ->add('estado')
+            ->add('resistenciaMagica')
+            ->add('reduccionDano')
+            ->add('arma')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            if(isset($request->request->all()['form']['id'])){
+                $plantilla->bind($request);
+                $personajePlantilla = $em->getRepository('app\IndexBundle\Entity\DD35\Personaje')->find($id->id);
+                $em->remove($personajePlantilla);
+                $em->flush();
+                $hola = "El personaje se borró correctamente";
+            }
+        }
+
+        $html = $this->container->get('templating')->render(
+            'index/masterDD35.html.twig', array('plantilla' => $plantilla->createView(),'form' => $var->createView(), 'hola' => $hola, 'tipo' => $tipo, 'link' => $link)
+        );
+
+        return new Response($html);
+    }
+
+    public function VampiroAction(Request $request){
+
+        $html = $this->container->get('templating')->render(
+            'index/rolManage.html.twig', array('hola' => '', 'juego' => 'VampiroMaster')
+        );
+
+        return new Response($html);
+    }
+
+    public function VampiroCreateAction(Request $request){ 
+        $hola = "";
+        $tipo = "";
+        $link = "crear";
         $ev = $this->get('doctrine.orm.vamp_entity_manager');
 
         $List = $ev->createQuery('SELECT p.ID, p.nombre FROM app\IndexBundle\Entity\Vampiro\\vPlantilla p ORDER BY p.ID ASC')->getResult();
@@ -269,7 +433,98 @@ class IndexController extends Controller{
         }
 
         $html = $this->container->get('templating')->render(
-            'index/masterVampiro.html.twig', array('plantilla' => $plantilla->createView(), 'form' => $var->createView(), 'hola' => $hola)
+            'index/masterVampiro.html.twig', array('plantilla' => $plantilla->createView(), 'form' => $var->createView(), 'hola' => $hola, 'tipo' => $tipo, 'link' => $link)
+        );
+
+        return new Response($html);
+    }
+
+    public function VampiroChangeAction(Request $request){ 
+        $hola = "";
+        $tipo = "oculto";
+        $link = "modificar";
+        $ev = $this->get('doctrine.orm.vamp_entity_manager');
+
+        $List = $ev->createQuery('SELECT p.ID, p.nombre FROM app\IndexBundle\Entity\Vampiro\\vPlantilla p ORDER BY p.ID ASC')->getResult();
+
+        for($i=0;$i<count($List);$i++){
+            $aux = $List[$i]['nombre'];
+            $plList[$List[$i]['ID']] = $aux;
+        }
+
+        $id = new idPlantilla();
+        $plantilla = $this->createFormBuilder($id)
+            ->add('id', 'choice', array('choices' => $plList, 'required' => true))
+            ->getForm();
+
+        $pj = new vPersonaje();
+        $var = $this->createFormBuilder($pj)
+            ->add('nombre')
+            ->add('clan')
+            ->add('generacion')
+            ->add('puntosVida')
+            ->add('armadura')
+            ->add('bonusFuerza')
+            ->add('bonusDestreza')
+            ->add('bonusResistencia')
+            ->add('bonusInteligencia')
+            ->add('bonusManipulacion')
+            ->add('bonusApariencia')
+            ->add('bonusPercepcion')
+            ->add('bonusAstucia')
+            ->add('bonusCarisma')
+            ->add('conciencia')
+            ->add('autocontrol')
+            ->add('coraje')
+            ->add('estado')
+            ->add('fuerzaVoluntad')
+            ->add('sangre')
+            ->add('arma')
+            ->add('habilidades', 'text')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            if(isset($request->request->all()['form']['id'])){
+                $plantilla->bind($request);
+                if($plantilla->isValid()){
+                    $personajePlantilla = $ev->getRepository('app\IndexBundle\Entity\Vampiro\vPlantilla')->find($id->id);
+                    $pj = $personajePlantilla;
+                    $tipo = "";
+                    $var = $this->createFormBuilder($pj)
+                        ->add('nombre')
+                        ->add('clan')
+                        ->add('generacion')
+                        ->add('puntosVida')
+                        ->add('armadura')
+                        ->add('bonusFuerza')
+                        ->add('bonusDestreza')
+                        ->add('bonusResistencia')
+                        ->add('bonusInteligencia')
+                        ->add('bonusManipulacion')
+                        ->add('bonusApariencia')
+                        ->add('bonusPercepcion')
+                        ->add('bonusAstucia')
+                        ->add('bonusCarisma')
+                        ->add('conciencia')
+                        ->add('autocontrol')
+                        ->add('coraje')
+                        ->add('estado')
+                        ->add('fuerzaVoluntad')
+                        ->add('sangre')
+                        ->add('arma')
+                        ->add('habilidades', 'text')
+                        ->getForm();
+                    }
+            }
+            $var->bind($request);
+            if($var->isValid()){
+                $ev->flush();
+                $hola = "Se modificó correctamente el personaje en la BD";
+            }
+        }
+
+        $html = $this->container->get('templating')->render(
+            'index/masterVampiro.html.twig', array('plantilla' => $plantilla->createView(), 'form' => $var->createView(), 'hola' => $hola, 'tipo' => $tipo, 'link' => $link)
         );
 
         return new Response($html);
