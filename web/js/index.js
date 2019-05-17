25f8d49fe1806fd64a8215cacbaeb80cc34a5798
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2019-05-17 13:10:02 +0200

    Añadiendo funcionalidad Javascript

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
+	if($("#form_game").val()==="DD35"){
+		$("#form_CD").parent().addClass("oculto");
+		$("#form_pj2").parent().addClass("oculto");
+		$("#form_spell").parent().addClass("oculto");
+		$("#form_skill").parent().addClass("oculto");
+		$("#form_action").change(function(){
+			$("#form_CD").parent().addClass("oculto");
+			$("#form_pj2").parent().addClass("oculto");
+			$("#form_spell").parent().addClass("oculto");
+			$("#form_skill").parent().addClass("oculto");
+			if($("#form_action").val()!="AutoHechizo" && $("#form_action").val()!="TiradaDificultad"){
+				$("#form_pj2").parent().removeClass("oculto");
+				if($("#form_action").val()==="TiradaEnfrentada"){
+					$("#form_skill").parent().removeClass("oculto");
+				}
+				if($("#form_action").val()==="HechizoObjetivo"){
+					$("#form_spell").parent().removeClass("oculto");
+				}
+			}else if($("#form_action").val()==="TiradaDificultad"){
+				$("#form_skill").parent().removeClass("oculto");
+				$("#form_CD").parent().removeClass("oculto");
+			}else{
+				$("#form_spell").parent().removeClass("oculto");
+			}
+		});
+	}
 	if($("#panelbody").text().length>11){
 		$("#panel").removeClass("oculto");
 	}
+	$('body').removeClass("oculto");
 });
