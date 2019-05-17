c264a1812f1b075bebd3ec120b99bb3616ee4edd
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2019-05-17 13:14:02 +0200

    Resolución de errores

--- web/js/index.js
+++ web/js/index.js
@@ -28,5 +28,5 @@ $(document).ready(function(){
 	if($("#panelbody").text().length>11){
 		$("#panel").removeClass("oculto");
 	}
-	$('body').removeClass("oculto");
+	$('.contenedor').removeClass("oculto");
 });
