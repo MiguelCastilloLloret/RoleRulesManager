b6b9656eaec2a1997c9188bc7f2775bdb8140d11
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2019-05-23 12:51:15 +0200

    Debug

--- web/js/index.js
+++ web/js/index.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 			}
 		});
 	}
-	if($("#paneltable").text().length>20){
+	if($("#paneltable").text().length>70){
 		$("#panel").removeClass("oculto");
 	}
 	$(".contenedor").removeClass("oculto");
