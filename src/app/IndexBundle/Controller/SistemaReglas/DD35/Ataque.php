2a7fedad7f675e0c4974521fb7750b0798a3a94c
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2019-05-23 14:34:11 +0200

    Mejora de la visualización de resultados

--- src/app/IndexBundle/Controller/SistemaReglas/DD35/Ataque.php
+++ src/app/IndexBundle/Controller/SistemaReglas/DD35/Ataque.php
@@ -4,14 +4,19 @@
 	$arma = $this->em->getRepository('app\IndexBundle\Entity\DD35\Arma')->findOneByNombre($pj1->arma);
 
 	$impacto = rand(1,20);
-	$critico = false;
+	//$critico = false;
+
+	$res = array('Atacante' => $executor->pj1, 'Arma' => $arma->nombre, 'Valor de Impacto' => "", 'Defensor' => $executor->pj2, 'Armadura Defensor' => $pj2->claseArmadura, 'Critico' => "No");
 	
 	if($arma->bonus == "Fuerza") $bonus = $pj1->bonusFuerza;
 	else $bonus = $pj1->bonusDestreza;
 
-	$danoFinal = $bonus;
+	$danoFinal = 0;
+	$res['Valor de Impacto'] = $impacto+$bonus;
 
 	if($impacto==20){
+		$res['Valor de Impacto'] = $impacto;
+		$danoFinal = $bonus;
 		$comprobacionCritico = rand(1,20);
 			$i = 0;
 		while ($i<$arma->dano){
@@ -20,10 +25,12 @@
 		}
 		if(($comprobacionCritico+$bonus)>$pj2->claseArmadura){
 			$danoFinal = $danoFinal*2;
-			$critico = true;
+			//$critico = true;
+			$res['Critico'] = "Si";
 		}
 	}
 	else if(($impacto+$bonus)>$pj2->claseArmadura){
+		$danoFinal = $bonus;
 		$i = 0;
 		while ($i<$arma->dano){
 			$danoFinal = $danoFinal + rand(1,$arma->dado);
@@ -31,12 +38,15 @@
 		}
 	}
 
-	if($danoFinal!=0) $var = $pj2->aplicar_efecto("Ataque", ($danoFinal*-1));
+	if($danoFinal!=0){
+		$var = $pj2->aplicar_efecto("Ataque", ($danoFinal*-1));
+		$res['Daño'] = $var*-1;
+	}
+	else $res['Daño'] = "Fallo";
 
-	
-
-	if($danoFinal == 0) $res = "El jugador ".$pj1->nombre." falla el ataque(Tirada ".$impacto."+".$bonus." vs CA ".$pj2->claseArmadura.").";
+	/*if($danoFinal == 0) $res = "El jugador ".$pj1->nombre." falla el ataque(Tirada ".$impacto."+".$bonus." vs CA ".$pj2->claseArmadura.").";
 	else if($impacto == 20 && $critico == true) $res = "El jugador ".$pj1->nombre." acierta un golpe crítico(Tirada ".$impacto."+".$bonus.", comprobación ".$comprobacionCritico."+".$bonus.") y causa ".($var*-1)." puntos de daño a ".$pj2->nombre.".";
 	else if($impacto == 20) $res = "El jugador ".$pj1->nombre." falla una oportunidad de crítico(Tirada ".$impacto."+".$bonus.", comprobación ".$comprobacionCritico."+".$bonus."). Su ataque causa ".($var*-1)." puntos de daño a ".$pj2->nombre.".";
 	else $res = "El jugador ".$pj1->nombre." acierta un ataque(Tirada ".$impacto."+".$bonus." vs CA ".$pj2->claseArmadura.") y causa ".($var*-1)." puntos de daño a ".$pj2->nombre.".";
+	*/
 ?>
