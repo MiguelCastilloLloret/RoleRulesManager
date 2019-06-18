3c7ba07f2998f7dc9cec1bd8eec6dcbbe535b2ff
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2019-06-18 14:11:03 +0200

    Solución de errores

--- src/app/IndexBundle/Controller/PartidaEncoder.php
+++ src/app/IndexBundle/Controller/PartidaEncoder.php
@@ -4,18 +4,15 @@ namespace app\IndexBundle\Controller;
 
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
-class Sha256Salted implements PasswordEncoderInterface
-{
+class Sha256Salted implements PasswordEncoderInterface{
 
-	$salt = 'salt5678901234567890123456789012';
+	public $salt = 'salt5678901234567890123456789012';
 
-    public function encodePassword($raw)
-    {
-        return hash('sha256', $this->salt . $raw); // Custom function for encrypt with sha256
+    public function encodePassword($raw){
+		return hash('sha256', $this->salt . $raw); // Custom function for encrypt with sha256
     }
 
-    public function isPasswordValid($encoded, $raw)
-    {
+    public function isPasswordValid($encoded, $raw){
         return $encoded === $this->encodePassword($raw, $this->salt);
     }
 
